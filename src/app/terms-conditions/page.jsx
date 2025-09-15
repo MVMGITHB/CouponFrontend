6c4322a13a4445f58d39389c99185637864001d4
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-09-15 13:26:42 +0530

    title changed

--- src/app/terms-conditions/page.jsx
+++ src/app/terms-conditions/page.jsx
@@ -1,27 +1,27 @@
 import TermsConditions from "@/components/termCondition/TermsConditions";
 
 export const metadata = {
-  title: 'Terms & Conditions | Job Ki Tyaari Guidelines',
+  title: 'Terms of Use - CouponsCulture',
   description:
-    'Read Job Ki Tyaari’s terms and conditions to understand our website policies, user responsibilities, and legal guidelines.',
+    'Read Coupons Cultures terms and conditions to understand our website policies, user responsibilities, and legal guidelines.',
   metadataBase: new URL('https://couponsculture.com'),
   alternates: {
     canonical: '/terms-conditions',
   },
   openGraph: {
-    title: 'Terms & Conditions | Job Ki Tyaari Guidelines',
+    title: 'Terms of Use - CouponsCulture',
     description:
-      'Understand our terms of use, disclaimers, limitations of liability, and your responsibilities as a user of Job Ki Tyaari.',
+      'Read Coupons Cultures terms and conditions to understand our website policies, user responsibilities, and legal guidelines.',
     url: 'https://couponsculture.com/terms-conditions',
-    siteName: 'Job Ki Tyaari',
+    siteName: 'CouponsCulture',
     locale: 'en_US',
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Terms & Conditions | Job Ki Tyaari Guidelines',
+    title: 'Terms & Conditions',
     description:
-      'Please read these terms and conditions carefully before using Job Ki Tyaari. Learn about our policies and legal practices.',
+      'Read Coupons Cultures terms and conditions to understand our website policies, user responsibilities, and legal guidelines.',
   },
   // Uncomment below to disallow indexing:
   // robots: {
