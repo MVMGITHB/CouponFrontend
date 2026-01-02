7b9227ca129d4e9a1549f31b7be4bcb17df5f266
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-01-02 13:17:08 +0530

    updated coupon culture 02-01-2026

--- src/components/helper/baseurl.jsx
+++ src/components/helper/baseurl.jsx
@@ -1,4 +1,4 @@
-// const base_url = "http://localhost:4000";
+// export const base_url = "http://localhost:5005";
 // const base_url = "https://coupanbackend-5iao.onrender.com";
 const base_url = "https://api.couponsculture.com";
 export const base_url1 = "https://user.couponsculture.com";
